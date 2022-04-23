1b3d6945c03a5c4e2e138989e1363bbd18497a95
Kári Rafn Karlsson <karirafn@gmail.com>
2022-04-23 17:25:31 +0000

    Added constants to Monster

--- CGSpringChallenge2022/Monster.cs
+++ CGSpringChallenge2022/Monster.cs
@@ -1,4 +1,8 @@
 ﻿public class Monster : Entity
 {
+    public const int Speed = 400;
+    public const int AggroRange = 5000;
+    public const int AttackRange = 300;
+
     public Monster(Entity entity) : base(entity) { }
 }
