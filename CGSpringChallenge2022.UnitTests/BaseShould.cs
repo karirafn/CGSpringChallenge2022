f87a8214a55f909312c9ede5497a37f9fd5d5730
Kári Rafn Karlsson <karirafn@gmail.com>
2022-04-23 09:52:28 +0000

    Added base class

---
+++ CGSpringChallenge2022.UnitTests/BaseShould.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace CGSpringChallenge2022.UnitTests
+{
+    public class BaseShould
+    {
+        [Fact]
+        public void Initialize()
+        {
+            // Arrange
+            int x = 5;
+            int y = 10;
+
+            // Act
+            Base sut = new Base(x, y);
+
+            // Assert
+            sut.Position.X.Should().Be(x);
+            sut.Position.Y.Should().Be(y);
+        }
+    }
+}
---
+++ CGSpringChallenge2022/Base.cs
@@ -0,0 +1,11 @@
+﻿public class Base
+{
+    public Base(int x, int y)
+    {
+        Position = new Point(x, y);
+    }
+
+    public Point Position { get; }
+    public int Health { get; set; }
+    public int Mana { get; set; }
+}
