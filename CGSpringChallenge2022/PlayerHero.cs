9c031db771a41f927e7753e1439ad59ee69e19e6
Kári Rafn Karlsson <karirafn@gmail.com>
2022-04-23 17:35:50 +0000

    Added shield and control spells

--- CGSpringChallenge2022/PlayerHero.cs
+++ CGSpringChallenge2022/PlayerHero.cs
@@ -7,24 +7,40 @@ public class PlayerHero : Entity
     private const string WAIT = nameof(WAIT);
     private const string MOVE = nameof(MOVE);
     private const string WIND = nameof(WIND);
+    private const string SHIELD = nameof(SHIELD);
+    private const string CONTROL = nameof(CONTROL);
 
     public PlayerHero(Entity entity) : base(entity) { }
 
     public void Wait() => Console.WriteLine(WAIT);
 
-    public void Move(IPositionable? location)
+    public void Move(IPositionable destination) => Move(destination?.Position);
+    public void Move(Point? destination)
     {
-        if (location is null)
+        if (destination is null)
             return;
 
-        Console.WriteLine($"{MOVE} {location?.Position.X} {location?.Position.Y}");
+        Console.WriteLine($"{MOVE} {destination?.X} {destination?.Y}");
     }
 
+    public void Wind(IPositionable direction) => Move(direction?.Position);
     public void Wind(Point? direction)
     {
         if (direction is null)
             return;
 
         Console.Write($"{WIND} {direction?.X} {direction?.Y}");
+    }
+
+    public void Shield(int id)
+        => Console.WriteLine($"{SHIELD} {id}");
+
+    public void Control(int id, IPositionable destination) => Control(id, destination?.Position);
+    public void Control(int id, Point? destination)
+    {
+        if (destination is null)
+            return;
+
+        Console.WriteLine($"{CONTROL} {id} {destination?.X} {destination?.Y}");
     }
 }
