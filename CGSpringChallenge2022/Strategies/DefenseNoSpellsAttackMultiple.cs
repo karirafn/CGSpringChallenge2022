1e069711ead1484624ce5c3c00fb26e6a8c8907c
Kári Rafn Karlsson <karirafn@gmail.com>
2022-04-23 17:19:26 +0000

    Renamed strategy

---
+++ CGSpringChallenge2022/Strategies/DefenseNoSpellsAttackMultiple.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CGSpringChallenge2022.Strategies
+{
+    public class DefenseNoSpellsAttackMultiple : IStrategy
+    {
+        private readonly Game _game;
+
+        public DefenseNoSpellsAttackMultiple(Game game)
+        {
+            _game = game;
+        }
+
+        public void PerformActions()
+        {
+            List<PlayerHero> heroes = _game.GetPlayerHeroes().ToList();
+            List<Enemy> enemies = _game.GetEnemiesSortedByDistanceFromBase().ToList();
+
+            if (!enemies.Any())
+                heroes.ForEach(h => h.Move(_game.PlayerBase));
+
+            if (enemies.Count == 1)
+                heroes.ForEach(h => h.Move(enemies[0]));
+
+            if (enemies.Count == 2)
+            {
+                heroes[0].Move(enemies.First());
+                for (int i = 0; i < 2; i++)
+                {
+                    heroes[i].Move(enemies[i]);
+                }
+            }
+
+            if (enemies.Count > 2)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    heroes[i].Move(enemies[i]);
+                }
+            }
+        }
+    }
+}
