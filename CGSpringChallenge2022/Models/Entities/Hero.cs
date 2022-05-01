352a0cdcae2463fcd0bca62478ec887ebbf22de5
Kári Rafn Karlsson <karirafn@gmail.com>
2022-05-01 11:00:18 +0000

    Added speed to hero class

--- CGSpringChallenge2022/Models/Entities/Hero.cs
+++ CGSpringChallenge2022/Models/Entities/Hero.cs
@@ -3,6 +3,7 @@
     public const int Visibility = 2200;
     public const int Range = 800;
     public const int Damage = 2;
+    public const int Speed = 800;
 
     public Hero(int id, EntityType type, Point position, int shield, bool isControlled)
         : base(id, type ,position, shield, isControlled)
